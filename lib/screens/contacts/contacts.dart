27f4ba2a282507c5caed643350156edcf6a30acb
syth0le <chdan565@gamil.com>
2020-11-10 02:27:42 +0300

    Contacts_Screen

--- lib/screens/contacts/contacts.dart
+++ lib/screens/contacts/contacts.dart
@@ -30,7 +30,48 @@ class ContactsScreen extends StatelessWidget {
           ],
           backgroundColor: Colors.white,
         ),
-        body: Container(),
+        body: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            // color: Colors.red,
+            // height: MediaQuery.of(context).size.height -
+            //     (MediaQuery.of(context).size.height / 6.5),
+            height: double.infinity,
+            width: MediaQuery.of(context).size.width -
+                (MediaQuery.of(context).size.height / 10),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 20),
+                  child: ContactsInput(),
+                ),
+                Expanded(
+                  child: ListView(
+                    scrollDirection: Axis.vertical,
+                    children: <Widget>[
+                      ContactContainer(),
+                      ContactContainer(),
+                      ContactContainer(),
+                      ContactContainer(),
+                      ContactContainer(),
+                      ContactContainer(),
+                      ContactContainer(),
+                      ContactContainer(),
+                      ContactContainer(),
+                      ContactContainer(),
+                      ContactContainer(),
+                      ContactContainer(),
+                      ContactContainer(),
+                      ContactContainer(),
+                      ContactContainer(),
+                      ContactContainer(),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
         bottomNavigationBar: new BottomNavigationBar(
           //   currentIndex: index,
           // onTap: (int index) {
@@ -66,6 +107,130 @@ class ContactsScreen extends StatelessWidget {
           unselectedItemColor: kAllColor,
           // onTap: _onItemTapped,
         ));
+  }
+}
+
+class ContactContainer extends StatelessWidget {
+  const ContactContainer({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+      child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.1),
+                spreadRadius: 1,
+                blurRadius: 1,
+                offset: Offset(0, 6), // changes position of shadow
+              ),
+            ],
+          ),
+          child: Row(
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 50,
+                width: 48,
+                decoration: new BoxDecoration(
+                  color: Color(0xFFF5F7FA),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: new Center(
+                  child: new Text(
+                    "A",
+                    style: GoogleFonts.manrope(
+                        textStyle: TextStyle(fontSize: 17, color: kAllColor)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                    // decoration: new BoxDecoration(color: Colors.blue),
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('To:',
+                        style: GoogleFonts.manrope(
+                          textStyle: TextStyle(
+                            fontSize: 12.0,
+                            color: Color.fromRGBO(155, 155, 155, 1.0),
+                          ),
+                        )),
+                    Text('Ann Nielsen',
+                        style: GoogleFonts.manrope(
+                          textStyle: TextStyle(
+                            fontSize: 16.0,
+                            color: kAllColor,
+                          ),
+                        )),
+                    Text('nielsen.ann@gmail.com',
+                        style: GoogleFonts.manrope(
+                          textStyle: TextStyle(
+                            fontSize: 12.0,
+                            color: Color.fromRGBO(155, 155, 155, 1.0),
+                          ),
+                        )),
+                  ],
+                )),
+              ),
+            ],
+          )),
+    );
+  }
+}
+
+class ContactsInput extends StatelessWidget {
+  const ContactsInput({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        decoration: InputDecoration(
+      prefixIcon: Padding(
+        padding: EdgeInsets.fromLTRB(15, 5, 15, 5),
+        child: SizedBox(
+          width: 25,
+          height: 25,
+          child: SvgPicture.asset(
+            'assets/icons/find_contacts.svg',
+            color: kPrimaryColor,
+          ),
+        ),
+      ),
+      border: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: kAllColor,
+        ),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: kPrimaryColor,
+        ),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Color(0xFF0070BA),
+        ),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+    ));
   }
 }
 
